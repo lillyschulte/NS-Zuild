af905cd574b5314a8d7a7e089ae055bbcdfed211
lillyschulte <rickschulte@hotmail.nl>
2022-10-11 09:41:28 +0200

    Bericht module test

--- main.py
+++ main.py
@@ -4,7 +4,22 @@
 
 import time
 
-tijd = time.strftime("%H:%M:%S", time.localtime())
-datum = time.strftime("%D", time.localtime())
-print(datum)
-print(tijd)
+def zuil_bericht(msg, username, station):
+    tijd = time.strftime("%H:%M:%S", time.localtime())
+    datum = time.strftime("%D", time.localtime())
+    f = open("NS-bericht.txt", "a")
+    f.write(msg + "\n" + datum + "\n" + tijd + "\n" + username + "\n" + station + "\n")
+    f.close()
+    print("success")
+
+def testcode():
+    devmsg = input("msg")
+    devusername = input("username")
+    devstation = input("station")
+    zuil_bericht(devmsg, devusername, devstation)
+
+testcode()
+
+
+
+
