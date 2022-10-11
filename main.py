a28f0c0dc051854eb0495554e294fcc51d6a6166
lillyschulte <rickschulte@hotmail.nl>
2022-10-11 09:26:09 +0200

    Time module

---
+++ main.py
@@ -0,0 +1,10 @@
+#   NS Zuil Project main file
+#   Rick Schulte
+#   11/10/2022
+
+import time
+
+tijd = time.strftime("%H:%M:%S", time.localtime())
+datum = time.strftime("%D", time.localtime())
+print(datum)
+print(tijd)
