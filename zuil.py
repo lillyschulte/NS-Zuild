f6c150c6d3e6d0d57b4a4639d234f153c4640211
lillyschulte <rickschulte@hotmail.nl>
2023-01-20 18:07:11 +0100

    Api test en GUI

--- zuil.py
+++ zuil.py
@@ -1,7 +1,8 @@
+import tkinter as tk
 import psycopg2
+import requests
 
 def display_moderated_messages():
-    location = input("Select a location (Utrecht[0], Amsterdam[1], Groningen[2]): ")
     connection = psycopg2.connect(
         host="127.0.0.1",
         database="NS-zuildDB",
@@ -15,4 +16,30 @@ def display_moderated_messages():
     for message in moderated_messages:
         print(f"Bericht: {message[1]}\nDatum: {message[2]}\nTijd: {message[3]}\nGebruiker: {message[4]}\nStation: {message[5]}\nModerator: {message[6]}\nModerator email: {message[7]}\nModerated_date: {message[8]}\nModerated_time: {message[9]}")
 
-display_moderated_messages()
+def get_weather(location):
+    # Code to make API call and retrieve weather information for the selected location
+    # Example using OpenWeatherMap API
+    api_key = "343670d3dfdfa8265b8024b1498731b3"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}"
+    response = requests.get(url)
+    weather_data = response.json()
+    print(weather_data)
+
+root = tk.Tk()
+root.configure(bg='yellow')
+root.title("Moderated Messages")
+
+location_label = tk.Label(root, text="Select a location:", bg='yellow')
+location_label.pack()
+
+location = tk.StringVar(value="Utrecht")
+location_dropdown = tk.OptionMenu(root, location, "Utrecht", "Amsterdam", "Groningen")
+location_dropdown.pack()
+
+weather_button = tk.Button(root, text="Check Weather", command=lambda: get_weather(location.get()))
+weather_button.pack()
+
+display_button = tk.Button(root, text="Display Moderated Messages", command=display_moderated_messages)
+display_button.pack()
+
+root.mainloop()
